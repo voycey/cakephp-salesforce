69f14c815ec348b4cdbb22725387650e025caa42
gregs <git@zuluru.org>
2017-08-29 14:10:01 -0400

    Simplified caching mechanism

--- src/Model/Table/SalesforcesTable.php
+++ src/Model/Table/SalesforcesTable.php
@@ -5,8 +5,7 @@ use Salesforce\Model\Entity\Salesforce;
 use Salesforce\ORM\SalesforceQuery;
 use Salesforce\ORM\SalesforceTable;
 use Cake\Cache\Cache;
-use Cake\Utility\Xml;
-use Cake\Utility\Hash;
+use Cake\Log\LogTrait;
 
 /**
  * Questions Model
@@ -17,18 +16,20 @@ use Cake\Utility\Hash;
  */
 class SalesforcesTable extends SalesforceTable
 {
-    public $schema = array();
-    public $updatable_fields = array();
-    public $selectable_fields = array();
+    use LogTrait;
+
+    private $_fields = [];
 
     public static function defaultConnectionName() {
         return 'salesforce';
     }
+
     /**
      * Initialize method
      *
      * @param  array $config The configuration for the Table.
      * @return void
+     * @throws \Exception
      */
     public function initialize(array $config)
     {
@@ -41,79 +42,71 @@ class SalesforcesTable extends SalesforceTable
         if(!empty($config['connection']->config()['my_wsdl'])) {
             $wsdl = CONFIG . DS  . $config['connection']->config()['my_wsdl'];
         } else {
-            throw new \Exception("You need to provide a WSDL");
+            throw new \Exception('You need to provide a WSDL');
         }
 
         $mySforceConnection = new \SforceEnterpriseClient();
-        $mySoapClient = $mySforceConnection->createConnection($wsdl);
+        $mySforceConnection->createConnection($wsdl);
 
         $sflogin = (array)Cache::read('salesforce_login', 'salesforce');
         if(!empty($sflogin['sessionId'])) {
             $mySforceConnection->setSessionHeader($sflogin['sessionId']);
-            $mySforceConnection->setEndPoint($sflogin['serverUrl']);
+            $mySforceConnection->setEndpoint($sflogin['serverUrl']);
         } else {
             try{
                 $mylogin = $mySforceConnection->login($config['connection']->config()['username'],$config['connection']->config()['password']);
-                $sflogin = array('sessionId' => $mylogin->sessionId, 'serverUrl' => $mylogin->serverUrl);
+                $sflogin = ['sessionId' => $mylogin->sessionId, 'serverUrl' => $mylogin->serverUrl];
                 Cache::write('salesforce_login', $sflogin, 'salesforce');
-            } catch (Exception $e) {
-                $this->log("Error logging into salesforce from Table - Salesforce down?");
+            } catch (\Exception $e) {
+                $this->log('Error logging into salesforce from Table - Salesforce down?');
             }
         }
-
 
         if (!$sObject = Cache::read($this->name.'_sObject', 'salesforce')) {
             $sObject = $mySforceConnection->describeSObject($this->name);
             Cache::write($this->name.'_sObject', $sObject, 'salesforce');
         }
 
+        $this->_fields = Cache::remember($this->name.'_schema', function () use ($sObject) {
+            $fields = [];
 
-        foreach ($sObject->fields as $field) {
-            if(substr($field->soapType,0,3) != "ens") { //we dont want type of ens
-                if(substr($field->soapType,4) == "int") {
-                    $type_name = "integer";
-                } elseif (substr($field->soapType,4) == "double") {
-                    $type_name = "float";
-                } elseif (substr($field->soapType,4) == "boolean") {
-                    $type_name = "boolean";
-                } elseif (substr($field->soapType,4) == "dateTime") {
-                    $type_name = "datetime";
-                } elseif (substr($field->soapType,4) == "date") {
-                    $type_name = "date";
-                } else {
-                    $type_name = "string";
-                }
-                if($field->updateable) {
-                    $this->updatable_fields[$field->name] = ['type' => $type_name, 'length' => $field->length, 'null' => $field->nillable];
-                    $this->selectable_fields[$field->name] = ['type' => $type_name, 'length' => $field->length, 'null' => $field->nillable];
-                } else {
-                    $this->selectable_fields[$field->name] = ['type' => $type_name, 'length' => $field->length, 'null' => $field->nillable];
+            foreach ($sObject->fields as $field) {
+                if(substr($field->soapType,0,3) != 'ens') { //we dont want type of ens
+                    if(substr($field->soapType,4) == 'int') {
+                        $type_name = 'integer';
+                    } elseif (substr($field->soapType,4) == 'double') {
+                        $type_name = 'float';
+                    } elseif (substr($field->soapType,4) == 'boolean') {
+                        $type_name = 'boolean';
+                    } elseif (substr($field->soapType,4) == 'dateTime') {
+                        $type_name = 'datetime';
+                    } elseif (substr($field->soapType,4) == 'date') {
+                        $type_name = 'date';
+                    } else {
+                        $type_name = 'string';
+                    }
+                    if($field->updateable) {
+                        $fields['updatable'][$field->name] = ['type' => $type_name, 'length' => $field->length, 'null' => $field->nillable];
+                    }
+                    $fields['selectable'][$field->name] = ['type' => $type_name, 'length' => $field->length, 'null' => $field->nillable];
                 }
             }
-        }
+
+            return $fields;
+        }, 'salesforce');
 
         //Cache select fields right away as most likely need them immediately
-        Cache::write($this->name.'_selectable_schema', $this->selectable_fields, 'salesforce');
-
-
-        if (!$this->schema(Cache::read($this->name.'_updatable_schema', 'salesforce'))) {
-            $this->schema($this->updatable_fields);
-            Cache::write($this->name.'_updatable_schema', $this->updatable_fields, 'salesforce');
-        }
-
+        $this->schema($this->_fields['selectable']);
     }
 
     public function beforeFind($event, $query, $options, $primary) {
-        if (!$this->schema(Cache::read($this->name.'_selectable_schema', 'salesforce'))) {
-            $this->schema($this->selectable_fields);
-            Cache::write($this->name.'_selectable_schema', $this->selectable_fields, 'salesforce');
-        }
+        $this->schema($this->_fields['selectable']);
     }
 
     public function beforeSave($event, $entity, $options) {
-        if (!$this->schema(Cache::read($this->name.'_updatable_schema', 'salesforce'))) {
-            $this->schema($this->updatable_fields);
-            Cache::write($this->name.'_updatable_schema', $this->updatable_fields, 'salesforce');
+        if($options['atomic']) {
+            throw new \Exception('Salesforce API does not support atomic transactions; set atomic to false.');
         }
+        $this->schema($this->_fields['updatable']);
     }
 }
